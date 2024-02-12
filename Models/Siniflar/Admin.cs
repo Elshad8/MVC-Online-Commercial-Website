831254f0ae0d16ab6cbc0fc1e7bfae2a9d83cc10
Elshad Feyzili <elshad.feyzili@gmail.com>
2024-02-12 14:27:31 +0400

    first commit

---
+++ Models/Siniflar/Admin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Admin
+    {
+        [Key]
+        public int Adminid { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string KullaniciAd { get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string Sifre { get; set; }
+
+
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
+        public string Yetki { get; set; }
+    }
+}
